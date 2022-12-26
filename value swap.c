3f742b640a0a4af665761dc7c627967dd5cee23e
Shawal Arefin <98606227+Shawal-Arefin@users.noreply.github.com>
2022-12-26 19:20:10 +0600

    Create value swap.c

---
+++ value swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = 1;
+    int b = 2;
+    printf("%d, %d\n", a, b);
+
+    //value switch
+
+    a = 2;
+    b = 1;
+
+    printf("%d, %d", a, b);
+
+    return 0;
+}

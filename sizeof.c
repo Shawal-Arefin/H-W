1cc50d8db73d232396b9e722dde9db1cda93f315
Shawal Arefin <98606227+Shawal-Arefin@users.noreply.github.com>
2022-12-30 05:42:22 +0600

    Create sizeof.c

---
+++ sizeof.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+   printf("size of char in byte: %d\n", sizeof(char));
+   printf("size of int in byte: %d\n", sizeof(int));
+   printf("size of float in byte: %d\n", sizeof(float));
+   printf("size of double in byte: %d\n", sizeof(double));
+   return 0;
+}

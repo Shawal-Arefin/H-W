c88a9fb3b3e6a3a45b204eee7b6bb478a002947a
Shawal Arefin <98606227+Shawal-Arefin@users.noreply.github.com>
2022-12-30 01:40:29 +0600

    Create ASCII.c

---
+++ ASCII.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char abc;
+    printf("Enter ur input: ");
+    scanf("%s", &abc);
+    printf("ASCII value of ur input is: %d", abc);
+    return 0;
+}
